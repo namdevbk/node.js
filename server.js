a61eacb53be33f1fc9f20d5dc24a8399cf4f3410
namdevbk <kendre.namdev123@gmail.com>
2016-05-03 12:02:03 +0530

    server.js

--- server.js
+++ server.js
@@ -6,33 +6,13 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-/*io.on('connection', function(socket){
-  console.log('a user connected');
-});*/
-/*io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});*/
-/*
-io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
-});
-*/
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log("Message : "+msg);
   });
 });
-
-/*io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});*/
-
 
 
 http.listen(3000, function(){
